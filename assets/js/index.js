6a8eaccf384af35529800e00423e2115ca6d9a0a
Jorge Espinoza <jorge.esp.ram@gmail.com>
2023-11-26 16:11:57 -0300

    primer commit

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+function cambiarBordeColor() {
+    let imagen = document.querySelector("#imgPerrito");
+    if (imagen.style.border === "2px solid red") {
+        imagen.style.border = "";
+    } else {
+        imagen.style.border = "2px solid red";
+    }
+}
+
+function procesarTickets() {
+    let inputSticker1 = document.querySelector("#inputSticker1").value;
+    let inputSticker2 = document.querySelector("#inputSticker2").value;
+    let inputSticker3 = document.querySelector("#inputSticker3").value;
+
+    let cantidadSticker1 = parseInt(inputSticker1);
+    let cantidadSticker2 = parseInt(inputSticker2);
+    let cantidadSticker3 = parseInt(inputSticker3);
+
+    let mensaje = document.querySelector("#mensaje");
+
+    //validar si los valores son numericos
+    if (isNaN(cantidadSticker1) || isNaN(cantidadSticker2) || isNaN(cantidadSticker3)) {
+        mensaje.innerText = "Solo se aceptan números";
+        return; //detener
+    }
+
+    //verificar mayor a 0
+    if (cantidadSticker1 < 0 || cantidadSticker2 < 0 || cantidadSticker3 < 0) {
+        mensaje.innerText = "Debe ingresar números mayores a 0";
+        return; //detener
+    }
+
+    let totalStickers = cantidadSticker1 + cantidadSticker2 + cantidadSticker3;
+
+    if (totalStickers <= 10) {
+        mensaje.innerText = "Llevas " + totalStickers + " stickers.";
+    } else {
+        mensaje.innerText = "Llevas demasiados stickers";
+    }
+}
+
